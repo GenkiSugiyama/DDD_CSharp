02bdf6d7f101795b7a4172e77dfb57039d8932cc
genki-sugiyama <gen-sugiyama@i-mobile.co.jp>
2023-07-09 16:03:50 +0900

    バッドコード記述まで

---
+++ DDD.WebForm/Common/CommonConst.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.WebForm.Common
+{
+    public static class CommonConst
+    {
+        public const string TemperatureUnitName = "℃";
+        public const int TemperatureDecimalPoint = 2;
+        public const string ConnectionString = @"Data Source=C:\Users\gen-sugiyama\SelfStudy\Udemy\LearnDDD\DDD.db;Version=3;";
+    }
+}
---
+++ DDD.WebForm/WeatherLatestView.cs
@@ -0,0 +1,56 @@
+﻿using DDD.WebForm.Common;
+using DDD.WebForm.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDD.WebForm
+{
+    public partial class WeatherLatestView : Form
+    {
+        public WeatherLatestView()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LatestButton_Click(object sender, EventArgs e)
+        {
+            var dt = WeatherSQLite.GetLatest(Convert.ToInt32(AreaIdTextBox.Text));
+
+            if (dt.Rows.Count > 0)
+            {
+                DataDateLabel.Text = dt.Rows[0]["DataDate"].ToString();
+                ConditionLabel.Text = dt.Rows[0]["Condition"].ToString();
+                TemperatureLabel.Text = CommonFunc.RoundString(
+                    Convert.ToSingle(dt.Rows[0]["Temperature"]), 
+                    CommonConst.TemperatureDecimalPoint
+                    ) 
+                    + CommonConst.TemperatureUnitName;
+            }
+        }
+
+        
+    }
+}
